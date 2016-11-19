97ef0cd3708b408bb2000ecc95a7f90aeccd87b0
Quilljou <quillzhou@gmail.com>
2016-11-19 12:44:56 +0800

    mirgrate to air

--- CityPicker/CityPicker.js
+++ CityPicker/CityPicker.js
@@ -1,17 +1,49 @@
 (function(){
     window.CityPicker = function()　{
         this.data = null;
+        this.defaultProvice = null;
     }
 
 
     CityPicker.prototype.init = function(element,options) {
+        this.element = element;
         extendDefault.call(this,options);
         getData.call(this);
+        if(this.data) {
+            this.defaultProvice = options.provice || first(this.data);
+            this.defaultCity = options.provice || first(this.data);
+            this.defaultCounty = options.county || this.defaultCity[0];
 
+
+            this.buildDom();
+
+
+        }
         return this;
     }
 
+
+    CityPicker.prototype.buildDom = function(){
+        this.decorate();
+
+
+    }
+
+    CityPicker.prototype.buildHeader = function() {
+    }
+
+    CityPicker.prototype.decorate = function() {
+        this.element.disabled = true;
+    }
+
+    CityPicker.prototype.bindEvent = function() {
+
+    }
+
     // private method
+    function first(obj) {
+        for (var a in obj) return obj[a];
+    }
 
     function extendDefault(o){
         var options = o || {};
@@ -26,7 +58,7 @@
 
         xhr.onload = function(){
             this.data = JSON.parse(xhr.responseText);
-            console.log(this.data);
+            console.log(first(this.data));
         }
     }
 
